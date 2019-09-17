75b3c442cd7f2fbaa3d95234cfe171cbda081832
Marco Aurélio <markinho9_7@hotmail.com>
2019-09-16 23:52:44 -0300

    Reconhecimento de imagens

--- program.py
+++ program.py
@@ -27,15 +27,23 @@ def main():
 
 def main2():
     img = cv2.imread("board4.jpeg")
-    # 889 x 877
     # img_resized = cv2.resize(img, (639, 627))
 
     board = find_pieces(img)
 
     print(board)
-    # send_positions(board)
+    # board = {
+    #     0: [1, 1, 1, 1, 1, 1, 1, 1],
+    #     1: [1, 1, 1, 0, 1, 1, 1, 1],
+    #     2: [0, 0, 0, 0, 0, 0, 0, 0],
+    #     3: [0, 0, 0, 1, 0, 0, 0, 0],
+    #     4: [0, 0, 0, 0, 0, 0, 0, 0],
+    #     5: [0, 0, 0, 0, 0, 0, 0, 0],
+    #     6: [1, 1, 1, 1, 1, 1, 1, 1],
+    #     7: [1, 1, 1, 1, 1, 1, 1, 1]
+    # }
 
-    # cv2.imshow('Chessboard Recognition', img_resized)
+    send_positions(board)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -58,7 +66,6 @@ def main3():
 
 
 def find_pieces(img):
-    img_copy = img.copy()
     board = {
         0: [0, 0, 0, 0, 0, 0, 0, 0],
         1: [0, 0, 0, 0, 0, 0, 0, 0],
@@ -70,53 +77,45 @@ def find_pieces(img):
         7: [0, 0, 0, 0, 0, 0, 0, 0]
     }
     square_size = 93
-    offset_x = 13
-    offset_y = 17
-
-    count = 0
+    offset_x = 17
+    offset_y = 21
 
     for y in range(0, 8):
         for x in range(0, 8):
             start_y = offset_y + square_size * y
             start_x = offset_x + square_size * x
-            end_y = start_y + square_size
-            end_x = start_x + square_size
+            end_y = start_y + square_size - 10
+            end_x = start_x + square_size - 10
 
-            cv2.rectangle(img_copy, (start_y, start_x), (end_y, end_x), (0, 0, 255), 1, 8, 0)
+            # cv2.rectangle(img, (start_y, start_x), (end_y, end_x), (0, 0, 255), 1, 8, 0)
             roi = img[start_y: end_y, start_x: end_x]
-            # cv2.imshow(f"{count}", roi)
-            # cv2.imwrite("roi.png", roi)
-            count += 1
 
-            # roi = cv2.imread("roi.png")
             hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-
-            lower_color = np.array([155, 5, -7])
-            upper_color = np.array([175, 25, 73])
-            mask1 = cv2.inRange(hsv, lower_color, upper_color)
-
-            lower_color = np.array([-10, -10, 214])
-            upper_color = np.array([10, 10, 294])
-            mask2 = cv2.inRange(hsv, lower_color, upper_color)
-            # res = cv2.bitwise_and(roi, roi, mask=mask)
-
-            contours1, _ = cv2.findContours(mask1, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-            cv2.drawContours(roi, contours1, -1, (0, 0, 255), 2)
-
-            contours2, _ = cv2.findContours(mask2, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-            cv2.drawContours(roi, contours2, -1, (0, 255, 0), 2)
+            contours1, contours2 = get_contours(hsv)
 
             if len(contours1) or len(contours2):
-                board[y][x] = 1
+                board[8 - y - 1][8 - x - 1] = 1
 
-            # if (y == 1 and x == 2) or (y == 1 and x == 4):
-                cv2.imshow(f"image{count}", roi)
-                # cv2.imshow(f"mask1{count}", mask1)
-                # cv2.imshow(f"mask2{count}", mask2)
-                # cv2.imshow(F"result{count}", res)
-
-    # cv2.imshow("rectangles", img_copy)
+    # cv2.imshow("rectangles", img)
     return board
+
+
+def get_contours(hsv):
+    lower_color = np.array([155, 5, -7])
+    upper_color = np.array([175, 25, 73])
+    mask1 = cv2.inRange(hsv, lower_color, upper_color)
+
+    contours1, _ = cv2.findContours(mask1, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    # cv2.drawContours(roi, contours1, -1, (0, 0, 255), 2)
+
+    lower_color = np.array([-10, -10, 214])
+    upper_color = np.array([10, 10, 294])
+    mask2 = cv2.inRange(hsv, lower_color, upper_color)
+
+    contours2, _ = cv2.findContours(mask2, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    # cv2.drawContours(roi, contours2, -1, (0, 255, 0), 2)
+
+    return contours1, contours2
 
 
 def histogram(img):

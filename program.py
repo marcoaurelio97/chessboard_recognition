a7fe30eb3c829abdb844338c872c1f6ab11710db
Marco Aurélio <markinho9_7@hotmail.com>
2019-09-19 00:28:14 -0300

    Reconhecimento finalizado

--- program.py
+++ program.py
@@ -5,6 +5,7 @@ import glob
 import requests
 from functions import send_positions
 from matplotlib import pyplot as plt
+import time
 
 
 def main():
@@ -26,8 +27,8 @@ def main():
 
 
 def main2():
-    img = cv2.imread("board4.jpeg")
-    # img_resized = cv2.resize(img, (639, 627))
+    img = cv2.imread("teste10.jpg")
+    img = cv2.resize(img, (475, 485))
 
     board = find_pieces(img)
 
@@ -43,7 +44,7 @@ def main2():
     #     7: [1, 1, 1, 1, 1, 1, 1, 1]
     # }
 
-    send_positions(board)
+    # send_positions(board)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -76,44 +77,52 @@ def find_pieces(img):
         6: [0, 0, 0, 0, 0, 0, 0, 0],
         7: [0, 0, 0, 0, 0, 0, 0, 0]
     }
-    square_size = 93
-    offset_x = 17
-    offset_y = 21
+    square_size = 61
+    offset_x = 7
+    offset_y = 6
 
     for y in range(0, 8):
         for x in range(0, 8):
             start_y = offset_y + square_size * y
             start_x = offset_x + square_size * x
-            end_y = start_y + square_size - 10
-            end_x = start_x + square_size - 10
+            end_y = start_y + square_size - 20
+            end_x = start_x + square_size - 20
 
-            # cv2.rectangle(img, (start_y, start_x), (end_y, end_x), (0, 0, 255), 1, 8, 0)
+            cv2.rectangle(img, (start_y, start_x), (end_y, end_x), (0, 0, 255), 1, 8, 0)
             roi = img[start_y: end_y, start_x: end_x]
 
             hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
             contours1, contours2 = get_contours(hsv)
 
-            if len(contours1) or len(contours2):
-                board[8 - y - 1][8 - x - 1] = 1
+            cv2.imwrite(f"testes/{y}{x}roi.jpg", roi)
+            cv2.imwrite(f"testes/{y}{x}hsv.jpg", hsv)
 
-    # cv2.imshow("rectangles", img)
+            # if y == 6:
+            #     cv2.imshow(f"{y}{x}roi", roi)
+            #     cv2.imshow(f"{y}{x}hsv", hsv)
+            #     print(f"{len(contours1)} {len(contours2)}")
+
+            if len(contours1) > 0 or len(contours2) > 0:
+                board[8 - y - 1][x] = 1
+
+    cv2.imshow("rectangles", img)
     return board
 
 
 def get_contours(hsv):
-    lower_color = np.array([155, 5, -7])
-    upper_color = np.array([175, 25, 73])
-    mask1 = cv2.inRange(hsv, lower_color, upper_color)
+    lower_color = np.array([14, 46, 55])  # 14 108 55
+    upper_color = np.array([34, 200, 258])  # 34 128 135
+    white_mask = cv2.inRange(hsv, lower_color, upper_color)
 
-    contours1, _ = cv2.findContours(mask1, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    # cv2.drawContours(roi, contours1, -1, (0, 0, 255), 2)
+    contours1, _ = cv2.findContours(white_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(hsv, contours1, -1, (0, 0, 255), 2)
 
-    lower_color = np.array([-10, -10, 214])
-    upper_color = np.array([10, 10, 294])
-    mask2 = cv2.inRange(hsv, lower_color, upper_color)
+    lower_color = np.array([90, 67, -30])
+    upper_color = np.array([110, 87, 50])
+    black_mask = cv2.inRange(hsv, lower_color, upper_color)
 
-    contours2, _ = cv2.findContours(mask2, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    # cv2.drawContours(roi, contours2, -1, (0, 255, 0), 2)
+    contours2, _ = cv2.findContours(black_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(hsv, contours2, -1, (0, 255, 0), 2)
 
     return contours1, contours2
 
@@ -128,7 +137,14 @@ def histogram(img):
     plt.show()
 
 
+def clear_files():
+    files = glob.glob("testes/*")
+    for f in files:
+        os.remove(f)
+
+
 if __name__ == "__main__":
+    clear_files()
     # main()  #webcam
     main2()  #photo
     # main3()  #cellphone
